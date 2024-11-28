9d1482b376e9430011ed0c0b746718f5ffc1368d
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2024-11-28 19:22:32 -0300

    Iniciando o desenvolvimento da page Home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import MyButton from './components/Button';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './pages/Student/home.jsx'
+import Home from './pages/Student/home.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MyButton colorButton="black" text="Cadastrar"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './css/NavBar.css';
+
+export default function NavBar(){
+    return(
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="search-container">
+                    <input 
+                        type="text" 
+                        className="search-input" 
+                        placeholder="Digite o nome do curso que quer encontrar">
+                    </input>
+                    <button className="search-button">Pesquisar</button>
+                </div>
+
+                <div className="user-profile">
+                    <img
+                        src="https://via.placeholder.com/40"
+                        alt="Avatar do usuário"
+                        className="user-avatar"
+                    />
+                    <span className="user-name">Pedrina <br></br> 
+                    <span className="user-role">Aluno(a)</span>
+                    </span>
+                </div>
+            </div>
+        </nav>
+    )
+}
