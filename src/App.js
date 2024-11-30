af0899ba86499aef3a6cf1c9b218a07a0ce955ca
analayza <analayza00@gmail.com>
2024-11-30 19:35:18 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './pages/Student/home.jsx'
 import Home from './pages/Student/home.jsx';
+import ProfileConfiguration from './pages/Student/ProfileConfiguration.jsx';
 
 function App() {
   return (
