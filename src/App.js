a3d43f94d38f5d869bd8e633e4e99424dd47de49
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2024-11-30 20:01:17 -0300

    Modificação

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './pages/Student/home.jsx'
 import Home from './pages/Student/home.jsx';
+import ProfileConfiguration from './pages/Student/ProfileConfiguration'
 
 function App() {
   return (
-      <Router>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/profile' element={<ProfileConfiguration/>}></Route>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/profile' element={<ProfileConfiguration/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
